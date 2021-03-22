68230c1a544626d8563613994d860a2f4166a1ce
samkatzen <76451377+samkatzen@users.noreply.github.com>
2021-03-22 17:28:51 -0400

    Add files via upload

---
+++ eyes.js
@@ -0,0 +1,15 @@
+eyes.js
+const balls = document.getElementsByClassName('ball');
+
+document.onmousemove = (event) => {
+  const x = (event.clientX * 100) / window.innerWidth + '%';
+  const y = (event.clientY * 100) / window.innerHeight + '%';
+
+for (var i=0; i < 2; i++){
+  balls[i].style.left = x;
+  balls[i].style.top = y;
+  balls[i].transform = 'translate(-' + x + ',-' + y + ')';
+}
+}
+
+
